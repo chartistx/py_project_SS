8a25aedaa2d15e5082a7d2f060d9768bbd41be74
RutaVildere <rutas2003@gmail.com>
2024-01-08 18:35:15 +0200

    Kods, kas iegūst vietu koordinātes

--- data/get_coordinates.py
+++ data/get_coordinates.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from geopy.geocoders import ArcGIS
+import os
 
 
-df=pd.read_csv('ss_flat_data.csv')
+#get path for data file
+path_parent = os.path.join(os.getcwd(),os.pardir)
+path_file = path_parent+'\\data\\ss_flat_data.csv'
+
+df=pd.read_csv(path_file)
 df_mod = df
 df_mod['Area'] = df['Area'].str.split().str.get(0)
 df_mod['Area'] = df_mod['Area'].str.replace('Maskavas','Maskavas Priešpilsēta')
@@ -17,16 +22,16 @@ size_list= len(df_values.tolist())
 data = []
 nom = ArcGIS()
 count=0
+print(str(count)+' / '+str(size_list))
 for each in df_list:
-    print(str(count)+' / '+str(size_list))
     reg_lat_long = [each[1],each[2]]# Region , Area
     result = nom.geocode('Latvia '+each[8])#forward Latvia + Reg_area
     reg_lat_long.append(result.latitude)
     reg_lat_long.append(result.longitude)
     data.append(reg_lat_long)
     count+=1
-
+    print(str(count) + ' / ' + str(size_list))
 
 df_final = pd.DataFrame(data,columns=['Region','Area','Latitude','Longitude'])
-df_final.to_excel('coordinates.xlsx')
+df_final.to_excel('coordinates.xlsx', index=False)
 print('Done')
